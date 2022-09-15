0e67411b5f78fe2219459316db24add503f30dea
dinaigul <dinaigul96@gmail.com>
2022-09-15 18:10:23 +0200

    use random

--- src/test/java/com/dinaigul/ExampleTest.java
+++ src/test/java/com/dinaigul/ExampleTest.java
@@ -3,14 +3,16 @@ package com.dinaigul;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt()>1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(23>20);
+        Assertions.assertTrue(new Random().nextInt()>20);
     }
 }
